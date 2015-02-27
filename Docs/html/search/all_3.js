8d199a9b1d6c78162683b78cce09d9c2dee098dd
Ed Doddridge <edoddridge@gmail.com>
2015-02-27 00:07:27 +0000

    TeX in docstrings works

--- Docs/html/search/all_3.js
+++ Docs/html/search/all_3.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['calculate_5fdensity',['calculate_density',['../classmitgcm_1_1core_1_1_density.html#a2218bb1f82fd92d2d828bc30a87b69f3',1,'mitgcm::core::Density']]],
-  ['calculate_5ftotrhotend',['calculate_TotRhoTend',['../classmitgcm_1_1core_1_1_density.html#aa8d1673db5cfbcd7fda25ad38a29b3a8',1,'mitgcm::core::Density']]],
-  ['compute_5fmasks',['compute_masks',['../classmitgcm_1_1core_1_1_grid.html#a5307132ea25e3b335e9be11ae38a5e03',1,'mitgcm::core::Grid']]],
+  ['calculate_5ftotrhotend',['calculate_TotRhoTend',['../classmitgcm_1_1core_1_1_density.html#ace1f408672554eb3871856cb8934aaa0',1,'mitgcm::core::Density']]],
+  ['compute_5fmasks',['compute_masks',['../classmitgcm_1_1core_1_1_grid.html#ad33f3e78699330079c7ae7dfbd5d7adb',1,'mitgcm::core::Grid']]],
   ['core_2epy',['core.py',['../core_8py.html',1,'']]]
 ];
--- Docs/html/search/all_8.js
+++ Docs/html/search/all_8.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['indices_5fand_5ffield',['indices_and_field',['../namespacemitgcm_1_1streamlines.html#ab0999e36574643a8ab149aef0fe3b283',1,'mitgcm::streamlines']]]
+  ['indices_5fand_5ffield',['indices_and_field',['../namespacemitgcm_1_1streamlines.html#ac29e083c45617ad0be326a556c549187',1,'mitgcm::streamlines']]]
 ];
