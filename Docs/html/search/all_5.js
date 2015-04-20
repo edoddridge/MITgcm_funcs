69039d210c73a618bcfb65300c7e558a54a041d2
Ed Doddridge <edoddridge@gmail.com>
2015-04-20 17:17:22 +0100

    Docs update

--- Docs/html/search/all_5.js
+++ Docs/html/search/all_5.js
@@ -4,6 +4,5 @@ var searchData=
   ['extract_5falong_5fpath2d',['extract_along_path2D',['../namespacemitgcm_1_1streamlines.html#a2e9f5324faa650d831d5b679e4301b2f',1,'mitgcm::streamlines']]],
   ['extract_5falong_5fpath3d',['extract_along_path3D',['../namespacemitgcm_1_1streamlines.html#a8280cf3b53590039168174aa93a0e4c5',1,'mitgcm::streamlines']]],
   ['extract_5falong_5fpath4d',['extract_along_path4D',['../namespacemitgcm_1_1streamlines.html#a290cc434dcffbedea22dcc00c75713dc',1,'mitgcm::streamlines']]],
-  ['extract_5fon_5fsurface',['extract_on_surface',['../namespacemitgcm_1_1functions.html#a299918b57fada07023fdbb6ac4d6fb6e',1,'mitgcm::functions']]],
-  ['extract_5fsurface',['extract_surface',['../namespacemitgcm_1_1functions.html#a0c92a8395bc703865868e6b0a2a35e49',1,'mitgcm::functions']]]
+  ['extract_5fon_5fsurface',['extract_on_surface',['../namespacemitgcm_1_1functions.html#a97ad44d662af0ab6cd58e2090a0dfe62',1,'mitgcm::functions']]]
 ];
