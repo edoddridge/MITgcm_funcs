8a5234cd8338370a929389622faa162c7991150c
Ed Doddridge <edoddridge@gmail.com>
2015-12-18 17:10:31 +0000

    Docs with ARCHER instructions

--- Docs/html/search/all_f.js
+++ Docs/html/search/all_f.js
@@ -6,8 +6,6 @@ var searchData=
   ['shift_5fvort_5fto_5ft',['shift_vort_to_T',['../namespacemitgcm_1_1functions.html#aed1104c67878e97d1fce0640dfd0bb26',1,'mitgcm::functions']]],
   ['shift_5fw_5fto_5ft',['shift_W_to_T',['../namespacemitgcm_1_1functions.html#a53542e46800d54165cbda624b24a1d83',1,'mitgcm::functions']]],
   ['show_5fvariables',['show_variables',['../namespacemitgcm_1_1functions.html#ab197f4884fc8a0ab7d38dc5a5cfd5eb2',1,'mitgcm::functions']]],
-  ['streaklines',['streaklines',['../namespacemitgcm_1_1streamlines.html#a493dab136e4b63582d7f6b77e7a9c644',1,'mitgcm::streamlines']]],
-  ['streaklines_5fmany',['streaklines_many',['../namespacemitgcm_1_1streamlines.html#a70dea9e5149dc20a5e0ee4d09ae9acc3',1,'mitgcm::streamlines']]],
   ['stream2',['stream2',['../namespacemitgcm_1_1streamlines.html#ac4d6c763bdac75600b3778f4e0ea3a31',1,'mitgcm::streamlines']]],
   ['stream3',['stream3',['../namespacemitgcm_1_1streamlines.html#a38690a408e43bb899018203363da090d',1,'mitgcm::streamlines']]],
   ['stream3_5fmany',['stream3_many',['../namespacemitgcm_1_1streamlines.html#a61bc94e18701e5365587ae77c3bcdfa3',1,'mitgcm::streamlines']]],
