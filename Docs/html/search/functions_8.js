f577d3fff676dd10667a964d5a9ecf0860f38744
Ed Doddridge <edoddridge@gmail.com>
2016-01-28 17:03:05 +0000

    added notebook on ARCHER instructions

--- Docs/html/search/functions_8.js
+++ Docs/html/search/functions_8.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['pathlines',['pathlines',['../namespacemitgcm_1_1streamlines.html#ab2862901dc9d4f96c6c6749dbe199e59',1,'mitgcm::streamlines']]],
+  ['pathlines_5ffor_5folic_5fxyt_5fani',['pathlines_for_OLIC_xyt_ani',['../namespacemitgcm_1_1streamlines.html#afa6ce36dee6bd42ca32e7bbbe84ea127',1,'mitgcm::streamlines']]],
   ['pathlines_5fmany',['pathlines_many',['../namespacemitgcm_1_1streamlines.html#a6e4718a7262d4c61651569dce9ff3dba',1,'mitgcm::streamlines']]],
   ['plt_5fmon_5fstats',['plt_mon_stats',['../namespacemitgcm_1_1functions.html#a6070f3c37fd5bc2f9f9e5c53dbd2d986',1,'mitgcm::functions']]]
 ];
