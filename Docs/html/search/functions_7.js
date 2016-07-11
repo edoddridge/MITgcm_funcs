ca7a6c76123b74780b29d978ff9b6597381185d9
Ed Doddridge <edoddridge@gmail.com>
2016-07-11 10:05:56 +0100

    move cmap generation outside of OLIC

--- Docs/html/search/functions_7.js
+++ Docs/html/search/functions_7.js
@@ -1,5 +1,7 @@
 var searchData=
 [
+  ['numeric_5fglm_5fxyz',['numeric_GLM_xyz',['../namespacemitgcm_1_1streamlines.html#a14a0f036d78f38df2d9e6cac70f2b2bd',1,'mitgcm::streamlines']]],
+  ['numeric_5fglm_5fxyzt',['numeric_GLM_xyzt',['../namespacemitgcm_1_1streamlines.html#a61a2ee1fe3b060825286a4cdf7999e1e',1,'mitgcm::streamlines']]],
   ['numerics_5freplace_5fnans',['numerics_replace_nans',['../namespacemitgcm_1_1functions.html#a4f5e2c48ffb91f133837e5fcae1eb143',1,'mitgcm::functions']]],
   ['numerics_5ftake_5fd_5fdx',['numerics_take_d_dx',['../classmitgcm_1_1core_1_1_tracerpoint__field.html#abdbf0d953baced17806d26ed4fedad12',1,'mitgcm::core::Tracerpoint_field']]],
   ['numerics_5ftake_5fd_5fdy',['numerics_take_d_dy',['../classmitgcm_1_1core_1_1_tracerpoint__field.html#aa7104af7db145561e258c97c96c1be1b',1,'mitgcm::core::Tracerpoint_field']]]
